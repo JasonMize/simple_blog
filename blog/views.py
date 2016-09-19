6387fe1eeefdf94ddcd3888c681114534a2a05d8
Jason Mize <jasonemize@gmail.com>
2016-09-19 13:34:20 -0400

    added post model

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from .models import Post
+
 
 def index(request):
+    posts = Post.objects.all()
 
-    return render (request, "blog/index.html")
+    context = {
+        "posts" : posts
+    }
+
+
+    return render (request, "blog/index.html", context)
 
 
 
